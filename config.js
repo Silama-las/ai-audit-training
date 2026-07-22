074f13407a96783aeddb6a275f11d642f30b2837
Silama-las <aleksandrova.elizaveta.1410@gmail.com>
2026-07-22 14:51:12 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+window.QUIZ_CONFIG = {
+  firebase: {
+    apiKey: "AIzaSyBZZyr87OwSFOCo49T4bRco-T1IK3pJp58",
+    authDomain: "ai-audit-training.firebaseapp.com",
+    projectId: "ai-audit-training",
+    storageBucket: "ai-audit-training.firebasestorage.app",
+    messagingSenderId: "785567934867",
+    appId: "1:785567934867:web:99afb2d92ee1ff99c66066",
+    measurementId: "G-JKS54DDXZF"
+  
+  },
+  voteUrl: "https://ваш_логин.github.io/ai-audit-training/vote.html"
+};
