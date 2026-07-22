853b26193c6819956d66c2d4a7abcb75b615db8d
Silama-las <aleksandrova.elizaveta.1410@gmail.com>
2026-07-22 15:41:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ window.QUIZ_CONFIG = {
   firebase: {
     apiKey: "AIzaSyBZZyr87OwSFOCo49T4bRco-T1IK3pJp58",
     authDomain: "ai-audit-training.firebaseapp.com",
+    databaseURL: "https://ai-audit-training-default-rtdb.europe-west1.firebasedatabase.app", // проверьте этот URL в консоли Firebase
     projectId: "ai-audit-training",
     storageBucket: "ai-audit-training.firebasestorage.app",
     messagingSenderId: "785567934867",
     appId: "1:785567934867:web:99afb2d92ee1ff99c66066",
     measurementId: "G-JKS54DDXZF"
-  
   },
-  voteUrl: "https://ваш_логин.github.io/ai-audit-training/vote.html"
+  voteUrl: "https://Silama-las.github.io/ai-audit-training/vote-ai-audit-training.html"
 };
